076cbf5c053690ef68616058ba0b594a2b7c0293
jeeinn <thinkwei2012@gmail.com>
2018-09-16 13:31:42 +0800

    init demo

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # hello-cordova-vuejs
-a cordova template with vuejs2.x
+A cordova template with vuejs2.x for creating cordova app.
+
+# How to use
+
+1. install with commond
+ ```shell
+cordova create hello-cordova-vuejs com.example.hello HelloC-V --template hello-cordova-vuejs
+```
+
+2. install cordova dependence
+```shell
+cd hello-cordova-vuejs/
+cordova platform add browser #cordava paltform add android ios
+cordova run browser
+```
+
+# Start with project
+
+install app dependence
+```shell
+cd hello-cordova-vuejs/myApp/
+npm install
+npm run dev
+```
+
+when build android or ios
+```shell
+cd hello-cordova-vuejs/myApp/
+npm run build
+cd ../
+cordova build android #cordava build ios
+```
+
+# Todo
+* Maybe use [Vue Material
+](http://github.com/vuematerial/vue-material) or [MDUI](https://github.com/zdhxiong/mdui) for UI 
+* add demo with Nativ Api
+
+# Contributing
+Please make sure to read the [Contributing Guide](https://github.com/vuematerial/vue-material/blob/master/.github/CONTRIBUTING.md) before making a pull request.
+
+# Donate
+Buy me a cup of coffee
+![donate](https://raw.githubusercontent.com/jeeinn/pics/master/alipay-wepay.png)
---
+++ template_src/myApp/src/components/HelloWorld.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://cordova.apache.org/"
+          target="_blank"
+        >
+          Cordava
+        </a>
+      </li>
+      <li>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Vue
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/jeeinn"
+          target="_blank"
+        >
+          Github
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Cordova & Vue.js App'
+    }
+  },
+  mounted: function () {
+    // test for cordova ready
+    var app = {
+      // Application Constructor
+      initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false)
+      },
+
+      // deviceready Event Handler
+      //
+      // Bind any cordova events here. Common events are:
+      // 'pause', 'resume', etc.
+      onDeviceReady: function () {
+        this.receivedEvent('deviceready')
+      },
+
+      // Update DOM on a Received Event
+      receivedEvent: function (id) {
+        console.log('Received Event: ' + id)
+      }
+    }
+    app.initialize()
+
+    setTimeout(function () { console.log('hello vue.') }, 5000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
